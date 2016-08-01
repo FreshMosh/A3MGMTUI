3717dd153d3fac7222c27aba05836f270e836820
FreshMosh <graf-von-tilly@freenet.de>
2016-08-01 16:49:28 +0200

    Test files

--- gui_master.py
+++ gui_master.py
@@ -6,8 +6,20 @@ import menu_bar as mb
 import MySQLdb
 
 
-root = Tk()
-root.title("SRV MGMT")
-frame_upper = tkinter.Frame(root)
-frame_lists = tkinter.Frame(root)
-frame_lower = tkinter.Frame(root)
+class gui_app():
+    def __init__(self, root):
+        self.root = root
+        self.root.title("SRV MGMT")
+        self.frame_upper = tkinter.Frame(root)
+        self.frame_lists = tkinter.Frame(root)
+        self.frame_lower = tkinter.Frame(root)
+        self.button_1 = ttk.Button(self.frame_lists, text="jlkjlk")
+        self.root.grid()
+        self.frame_lists.grid()
+
+
+
+if __name__ == '__main__':
+    root = Tk()
+    app = gui_app(root)
+    app.
